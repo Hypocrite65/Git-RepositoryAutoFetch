d5589810d38dcfd1bf29b48fbfad75e18eca1b33
hypocrite65 <QW3188801738@outlook.com>
2023-12-05 22:34:21 +0800

    [1]debug file add log.rtf; [2]the cnt of pull failed has bug

--- debug/Auto.bat
+++ debug/Auto.bat
@@ -1,4 +1,14 @@
 @echo off
-rem This is a "Hello World" program.
-echo Hello World!
-echo=
+
+rem This is a BAT file to run a shell script at a specific time.
+
+rem Set the time you want to run the shell script.
+rem In this example, the shell script will be run at 10:00 AM.
+set time=10:00 AM
+
+rem Set the path to the shell script.
+rem In this example, the shell script is located in the current directory.
+set script_path=AutoFetch_debug.sh
+
+rem Run the `at` command to schedule the shell script to run at the specified time.
+at %time% %script_path%
